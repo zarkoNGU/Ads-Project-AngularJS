399ccc0e134e1f8bb6078655184b62f0285c13cc
Elezar Ivanov <zarkongu@gmail.com>
2015-01-10 09:24:18 +0200

    Structure and successfuly connecting

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var app = angular.module('adsApp', ['ngRoute', 'ngResource']);
+
+app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net/api')
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {    
+        templateUrl: 'templates/home.html', 
+        controller: 'HomeCtrl'
+    });
+    $routeProvider.when('/login',{
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    });
+    $routeProvider.when('/register',{
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterCtrl'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });   
+}]);
---
+++ js/directives/left-sidebar.js
@@ -0,0 +1,9 @@
+app.directive('leftPublicSidebar', function() {
+
+    return {
+        controller: 'HomeCtrl',
+        restrict: 'E',
+        templateUrl: 'templates/public/left-sidebar.html',
+        replace: true,    
+    };
+});
